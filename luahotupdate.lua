e0e7b2a7b50b1d247f67ea76f1b49610a279fbcb
Jin Qing <jinq0123@163.com>
2016-07-04 17:35:23 +0800

    Change to Lua 5.3.

--- luahotupdate.lua
+++ luahotupdate.lua
@@ -1,45 +1,43 @@
-local HU = {}
+--[[
+Lua hot update for lua5.3.
+Update functions and keep data.
+From https://github.com/asqbtcupid/lua_hotupdate
+--]]
 
-function HU.FailNotify(...)
-	if HU.NotifyFunc then HU.NotifyFunc(...) end
-end
-function HU.DebugNofity(...)
-	if HU.DebugNofityFunc then HU.DebugNofityFunc(...) end
-end
+local HU = {
+	ModuleNameOfUpdateList = "",
+	OldCode = { },
+	ChangedFuncList = { },
+	VisitedSig = { },
 
-function HU.InitFileMap(RootPath)
-	for _, rootpath in pairs(RootPath) do
-		local file = io.popen("dir /S/B /A:A "..rootpath)
-		io.input(file)
-		for line in io.lines() do
-	   		local FileName = string.match(line,".*\\(.*)%.lua")
-	  	    if FileName ~= nil then
-	            if HU.FileMap[FileName] == nil then
-	            	HU.FileMap[FileName] = {}
-	        	end
-	        	local luapath = string.sub(line, #rootpath+2, #line-4)
-				luapath = string.gsub(luapath, "\\", ".")
-				HU.LuaPathToSysPath[luapath] = SysPath
-	        	table.insert(HU.FileMap[FileName], {SysPath = line, LuaPath = luapath})
-	    	end
-	    end
-	    file:close()
-	end
+	FakeENV = nil,
+	ENV = nil,
+	NewFakeT = nil,  -- Create a fake table.
+}
+
+-- The app can change these notify functions:
+--    HU.InfoNotify = function(msg) log_info(msg) end
+function HU.FailNotify(MsgStr)
+end
+function HU.InfoNotify(MsgStr)
+end
+function HU.DebugNotify(MsgStr)
 end
 
-function HU.InitFakeTable()
-	local meta = {}
-	HU.Meta = meta
-	local function FakeT() return setmetatable({}, meta) end
+-- Init HU.NewFakeT
+function HU.InitFakeTblCreator()
+	local meta = { }
+	HU.Meta = meta  -- HU.Meta is in HU.Protection.
+	local function NewFakeT() return setmetatable( { }, meta) end
 	local function EmptyFunc() end
-	local function pairs() return EmptyFunc end  
+	local function pairs() return EmptyFunc end
 	local function setmetatable(t, metaT)
-		HU.MetaMap[t] = metaT 
+		HU.MetaMap[t] = metaT
 		return t
 	end
 	local function require(LuaPath)
 		if not HU.RequireMap[LuaPath] then
-			local FakeTable = FakeT()
+			local FakeTable = NewFakeT()
 			HU.RequireMap[LuaPath] = FakeTable
 		end
 		return HU.RequireMap[LuaPath]
@@ -53,14 +51,20 @@ function HU.InitFakeTable()
 			return EmptyFunc
 		elseif k == "require" then
 			return require
+		elseif k == "table" then
+			return table
+		elseif k == "string" then
+			return string
+		elseif k == "math" then
+			return math
 		else
-			local FakeTable = FakeT()
+			local FakeTable = NewFakeT()
 			rawset(t, k, FakeTable)
-			return FakeTable 
+			return FakeTable
 		end
 	end
 	function meta.__newindex(t, k, v) rawset(t, k, v) end
-	function meta.__call() return FakeT(), FakeT(), FakeT() end
+	function meta.__call() return NewFakeT(), NewFakeT(), NewFakeT() end
 	function meta.__add() return meta.__call() end
 	function meta.__sub() return meta.__call() end
 	function meta.__mul() return meta.__call() end
@@ -68,16 +72,26 @@ function HU.InitFakeTable()
 	function meta.__mod() return meta.__call() end
 	function meta.__pow() return meta.__call() end
 	function meta.__unm() return meta.__call() end
+
+	-- No in lua5.1:
+	function meta.__idiv() return meta.__call() end
+	function meta.__band() return meta.__call() end
+	function meta.__bor() return meta.__call() end
+	function meta.__bxor() return meta.__call() end
+	function meta.__bnot() return meta.__call() end
+	function meta.__shl() return meta.__call() end
+	function meta.__shr() return meta.__call() end
+
 	function meta.__concat() return meta.__call() end
 	function meta.__eq() return meta.__call() end
 	function meta.__lt() return meta.__call() end
 	function meta.__le() return meta.__call() end
 	function meta.__len() return meta.__call() end
-	return FakeT
+	HU.NewFakeT = NewFakeT
 end
 
 function HU.InitProtection()
-	HU.Protection = {}
+	HU.Protection = { }
 	HU.Protection[setmetatable] = true
 	HU.Protection[pairs] = true
 	HU.Protection[ipairs] = true
@@ -85,92 +99,75 @@ function HU.InitProtection()
 	HU.Protection[require] = true
 	HU.Protection[HU] = true
 	HU.Protection[HU.Meta] = true
+	HU.Protection[math] = true
+	HU.Protection[string] = true
+	HU.Protection[table] = true
 end
 
-function HU.AddFileFromHUList()
-	package.loaded[HU.UpdateListFile] = nil
-	local FileList = require (HU.UpdateListFile)
-	HU.ALL = false
-	HU.HUMap = {}
-	for _, file in pairs(FileList) do
-		if file == "_ALL_" then
-			HU.ALL = true
-			for k, v in pairs(HU.FileMap) do
-				for _, path in pairs(v) do
-					HU.HUMap[path.LuaPath] = path.SysPath  	
-				end
-			end
-			return
-		end
-		if HU.FileMap[file] then
-			for _, path in pairs(HU.FileMap[file]) do
-				HU.HUMap[path.LuaPath] = path.SysPath  	
-			end
-		else
-			HU.FailNotify("HotUpdate can't not find "..file)
-		end
-	end
+function HU.Reload(ModuleName)
+	package.loaded[ModuleName] = nil
+	return require(ModuleName)
 end
 
 function HU.ErrorHandle(e)
-	HU.FailNotify("HotUpdate Error\n"..tostring(e))
+	HU.FailNotify("HotUpdate error: " .. tostring(e))
 	HU.ErrorHappen = true
 end
 
 function HU.BuildNewCode(SysPath, LuaPath)
 	io.input(SysPath)
 	local NewCode = io.read("*all")
-	if HU.ALL and HU.OldCode[SysPath] == nil then
-		HU.OldCode[SysPath] = NewCode
-		return
-	end
+	io.input():close()
 	if HU.OldCode[SysPath] == NewCode then
-		io.input():close()
 		return false
 	end
-	HU.DebugNofity(SysPath)
-	io.input(SysPath)  
-	local chunk = "--[["..LuaPath.."]] "
-	chunk = chunk..NewCode	
-	io.input():close()
-	local NewFunction = loadstring(chunk)
-	if not NewFunction then 
-  		HU.FailNotify(SysPath.." has syntax error.")  	
-  		collectgarbage("collect")
-  		return false
+	HU.InfoNotify("Update: " .. LuaPath .. " -> " .. SysPath)
+	local chunk = "--[[" .. LuaPath .. "]] " .. NewCode
+	local FakeENV = HU.NewFakeT()
+	local NewFunction = load(chunk, LuaPath, 't', FakeENV)  -- Lua5.2+
+	if not NewFunction then
+		HU.FailNotify(SysPath .. " has syntax error.")
+		collectgarbage("collect")
+		return false
+	end
+
+	HU.FakeENV = FakeENV
+	HU.MetaMap = { }
+	HU.RequireMap = { }
+	local NewObject
+	HU.ErrorHappen = false
+	xpcall( function() NewObject = NewFunction() end, HU.ErrorHandle)
+	if not HU.ErrorHappen then
+		HU.OldCode[SysPath] = NewCode
+		return true, NewObject
 	else
-		HU.FakeENV = HU.FakeT()
-		HU.MetaMap = {}
-		HU.RequireMap = {}
-		setfenv(NewFunction, HU.FakeENV)
-		local NewObject
-		HU.ErrorHappen = false
-		xpcall(function () NewObject = NewFunction() end, HU.ErrorHandle)
-		if not HU.ErrorHappen then 
-			HU.OldCode[SysPath] = NewCode
-			return true, NewObject
-		else
-	  		collectgarbage("collect")
-			return false
-		end
+		collectgarbage("collect")
+		return false
 	end
 end
 
 function HU.Travel_G()
-	local visited = {}
+	local visited = { }
 	visited[HU] = true
 	local function f(t)
 		if (type(t) ~= "function" and type(t) ~= "table") or visited[t] or HU.Protection[t] then return end
 		visited[t] = true
 		if type(t) == "function" then
-		  	for i = 1, math.huge do
+			for i = 1, math.huge do
 				local name, value = debug.getupvalue(t, i)
 				if not name then break end
+				if type(value) == "function" then
+					for _, funcs in ipairs(HU.ChangedFuncList) do
+						if value == funcs[1] then
+							debug.setupvalue(t, i, funcs[2])
+						end
+					end
+				end
 				f(value)
 			end
 		elseif type(t) == "table" then
 			f(debug.getmetatable(t))
-			for k,v in pairs(t) do
+			for k, v in pairs(t) do
 				f(k); f(v);
 				if type(v) == "function" or type(k) == "function" then
 					for _, funcs in ipairs(HU.ChangedFuncList) do
@@ -181,6 +178,7 @@ function HU.Travel_G()
 			end
 		end
 	end
+
 	f(_G)
 	local registryTable = debug.getregistry()
 	for _, funcs in ipairs(HU.ChangedFuncList) do
@@ -198,7 +196,7 @@ end
 function HU.ReplaceOld(OldObject, NewObject, LuaPath, From, Deepth)
 	if type(OldObject) == type(NewObject) then
 		if type(NewObject) == "table" then
-			HU.UpdateAllFunction(OldObject, NewObject, LuaPath, From, "") 
+			HU.UpdateAllFunction(OldObject, NewObject, LuaPath, From, "")
 		elseif type(NewObject) == "function" then
 			HU.UpdateOneFunction(OldObject, NewObject, LuaPath, nil, From, "")
 		end
@@ -208,8 +206,8 @@ end
 function HU.HotUpdateCode(LuaPath, SysPath)
 	local OldObject = package.loaded[LuaPath]
 	if OldObject ~= nil then
-		HU.VisitedSig = {}
-		HU.ChangedFuncList = {}
+		HU.VisitedSig = { }
+		HU.ChangedFuncList = { }
 		local Success, NewObject = HU.BuildNewCode(SysPath, LuaPath)
 		if Success then
 			HU.ReplaceOld(OldObject, NewObject, LuaPath, "Main", "")
@@ -218,38 +216,49 @@ function HU.HotUpdateCode(LuaPath, SysPath)
 				HU.ReplaceOld(OldObject, NewObject, LuaPath, "Main_require", "")
 			end
 			setmetatable(HU.FakeENV, nil)
-			HU.UpdateAllFunction(HU.ENV, HU.FakeENV, " ENV ", "Main", "")
+			HU.UpdateAllFunction(HU.ENV, HU.FakeENV, "ENV", "Main", "")
 			if #HU.ChangedFuncList > 0 then
 				HU.Travel_G()
 			end
 			collectgarbage("collect")
 		end
+	elseif HU.OldCode[SysPath] == nil then
+		io.input(SysPath)
+		HU.OldCode[SysPath] = io.read("*all")
+		io.input():close()
 	end
 end
 
+function HU.Debug(Deepth, HUFunName, Name, From)
+	HU.DebugNotify(string.format("%s%s name:%s from:%s",
+		Deepth, HUFunName, Name, From))
+end  -- FormatDebugMsg()
+
 function HU.ResetENV(object, name, From, Deepth)
-	local visited = {}
-	local function f(object, name)
+	local visited = { }
+	local function f(object, name, From, Deepth)
 		if not object or visited[object] then return end
 		visited[object] = true
+		HU.Debug(Deepth, "HU.ResetENV", name, From)
 		if type(object) == "function" then
-			HU.DebugNofity(Deepth.."HU.ResetENV", name, "  from:"..From)
-			xpcall(function () setfenv(object, HU.ENV) end, HU.FailNotify)
+			-- xpcall( function() setfenv(object, HU.ENV) end, HU.FailNotify)
 		elseif type(object) == "table" then
-			HU.DebugNofity(Deepth.."HU.ResetENV", name, "  from:"..From)
 			for k, v in pairs(object) do
-				f(k, tostring(k).."__key", " HU.ResetENV ", Deepth.."    " )
-				f(v, tostring(k), " HU.ResetENV ", Deepth.."    ")
+				f(k, tostring(k) .. "__key", "HU.ResetENV", Deepth .. "    ")
+				f(v, tostring(k), "HU.ResetENV", Deepth .. "    ")
 			end
 		end
 	end
-	f(object, name)
+	f(object, name, From, Deepth)
 end
 
+-- Get upvalues of OldFunction and update those of NewFunction.
 function HU.UpdateUpvalue(OldFunction, NewFunction, Name, From, Deepth)
-	HU.DebugNofity(Deepth.."HU.UpdateUpvalue", Name, "  from:"..From)
-	local OldUpvalueMap = {}
-	local OldExistName = {}
+	assert("function" == type(OldFunction))
+	assert("function" == type(NewFunction))
+	HU.Debug(Deepth, "HU.UpdateUpvalue", Name, From)
+	local OldUpvalueMap = { }
+	local OldExistName = { }
 	for i = 1, math.huge do
 		local name, value = debug.getupvalue(OldFunction, i)
 		if not name then break end
@@ -264,81 +273,84 @@ function HU.UpdateUpvalue(OldFunction, NewFunction, Name, From, Deepth)
 			if type(OldValue) ~= type(value) then
 				debug.setupvalue(NewFunction, i, OldValue)
 			elseif type(OldValue) == "function" then
-				HU.UpdateOneFunction(OldValue, value, name, nil, "HU.UpdateUpvalue", Deepth.."    ")
+				HU.UpdateOneFunction(OldValue, value, name, nil, "HU.UpdateUpvalue", Deepth .. "    ")
 			elseif type(OldValue) == "table" then
-				HU.UpdateAllFunction(OldValue, value, name, "HU.UpdateUpvalue", Deepth.."    ")
+				HU.UpdateAllFunction(OldValue, value, name, "HU.UpdateUpvalue", Deepth .. "    ")
 				debug.setupvalue(NewFunction, i, OldValue)
 			else
 				debug.setupvalue(NewFunction, i, OldValue)
 			end
 		else
-			HU.ResetENV(value, name, "HU.UpdateUpvalue", Deepth.."    ")
+			HU.ResetENV(value, name, "HU.UpdateUpvalue", Deepth .. "    ")
 		end
 	end
 end 
 
-function HU.UpdateOneFunction(OldObject, NewObject, FuncName, OldTable, From, Deepth)
-	if HU.Protection[OldObject] or HU.Protection[NewObject] then return end
-	if OldObject == NewObject then return end
-	local signature = tostring(OldObject)..tostring(NewObject)
+function HU.UpdateOneFunction(OldFunction, NewFunction, FuncName, OldTable, From, Deepth)
+	assert("function" == type(OldFunction))
+	assert("function" == type(NewFunction))
+	if HU.Protection[OldFunction] or HU.Protection[NewFunction] then return end
+	if OldFunction == NewFunction then return end
+	local signature = tostring(OldFunction) .. tostring(NewFunction)
 	if HU.VisitedSig[signature] then return end
 	HU.VisitedSig[signature] = true
-	HU.DebugNofity(Deepth.."HU.UpdateOneFunction "..FuncName.."  from:"..From)
-	if pcall(debug.setfenv, NewObject, getfenv(OldObject)) then
-		HU.UpdateUpvalue(OldObject, NewObject, FuncName, "HU.UpdateOneFunction", Deepth.."    ")
-		HU.ChangedFuncList[#HU.ChangedFuncList + 1] = {OldObject, NewObject, FuncName, OldTable}
-	end
+	HU.Debug(Deepth, "HU.UpdateOneFunction", FuncName, From)
+	-- if pcall(debug.setfenv, NewFunction, getfenv(OldFunction)) then
+		HU.UpdateUpvalue(OldFunction, NewFunction, FuncName, "HU.UpdateOneFunction", Deepth .. "    ")
+		HU.ChangedFuncList[#HU.ChangedFuncList + 1] = { OldFunction, NewFunction, FuncName, OldTable }
+	-- end
 end
 
 function HU.UpdateAllFunction(OldTable, NewTable, Name, From, Deepth)
 	if HU.Protection[OldTable] or HU.Protection[NewTable] then return end
 	if OldTable == NewTable then return end
-	local signature = tostring(OldTable)..tostring(NewTable)
+	local signature = tostring(OldTable) .. tostring(NewTable)
 	if HU.VisitedSig[signature] then return end
 	HU.VisitedSig[signature] = true
-	HU.DebugNofity(Deepth.."HU.UpdateAllFunction "..Name.."  from:"..From)
+	HU.Debug(Deepth, "HU.UpdateAllFunction", Name, From)
 	for ElementName, Element in pairs(NewTable) do
 		local OldElement = OldTable[ElementName]
 		if type(Element) == type(OldElement) then
 			if type(Element) == "function" then
-				HU.UpdateOneFunction(OldElement, Element, ElementName, OldTable, "HU.UpdateAllFunction", Deepth.."    ")
+				HU.UpdateOneFunction(OldElement, Element, ElementName, OldTable, "HU.UpdateAllFunction", Deepth .. "    ")
 			elseif type(Element) == "table" then
-				HU.UpdateAllFunction(OldElement, Element, ElementName, "HU.UpdateAllFunction", Deepth.."    ")
+				HU.UpdateAllFunction(OldElement, Element, ElementName, "HU.UpdateAllFunction", Deepth .. "    ")
 			end
 		elseif OldElement == nil and type(Element) == "function" then
-			if pcall(setfenv, Element, HU.ENV) then
+			-- if pcall(setfenv, Element, HU.ENV) then
 				OldTable[ElementName] = Element
-			end
+			-- end
 		end
 	end
-	local OldMeta = debug.getmetatable(OldTable)  
+	local OldMeta = debug.getmetatable(OldTable)
 	local NewMeta = HU.MetaMap[NewTable]
 	if type(OldMeta) == "table" and type(NewMeta) == "table" then
-		HU.UpdateAllFunction(OldMeta, NewMeta, Name.."'s Meta", "HU.UpdateAllFunction", Deepth.."    ")
+		HU.UpdateAllFunction(OldMeta, NewMeta, Name .. "'s Meta", "HU.UpdateAllFunction", Deepth .. "    ")
 	end
 end
 
-function HU.Init(UpdateListFile, RootPath, FailNotify, ENV)
-	HU.UpdateListFile = UpdateListFile
-	HU.HUMap = {}
-	HU.FileMap = {}
-	HU.NotifyFunc = FailNotify
-	HU.OldCode = {}
-	HU.ChangedFuncList = {}
-	HU.VisitedSig = {}
-	HU.FakeENV = nil
+-- ModuleNameOfUpdateList is the module name that returns the hot update list.
+function HU.Init(ModuleNameOfUpdateList, ENV)
+	assert("string" == type(ModuleNameOfUpdateList))
+	assert("table" == type(require(ModuleNameOfUpdateList)))
+
+	HU.ModuleNameOfUpdateList = ModuleNameOfUpdateList
 	HU.ENV = ENV or _G
-	HU.LuaPathToSysPath = {}
-	HU.InitFileMap(RootPath)
-	HU.FakeT = HU.InitFakeTable()
+	HU.InitFakeTblCreator()
 	HU.InitProtection()
-	HU.ALL = false
 end
 
 function HU.Update()
-	HU.AddFileFromHUList()
-	for LuaPath, SysPath in pairs(HU.HUMap) do
-		HU.HotUpdateCode(LuaPath, SysPath)
+	HU.DebugNotify("Update()")
+	local UpdateModuleNames = HU.Reload(HU.ModuleNameOfUpdateList)
+	local Path = package.path .. ";" .. package.cpath
+	for _, ModuleName in pairs(UpdateModuleNames) do
+		local SysPath, ErrMsg = package.searchpath(ModuleName, Path)
+		if SysPath then
+			HU.HotUpdateCode(ModuleName, SysPath)
+		else
+			HU.DebugNotify(ErrMsg)
+		end
 	end
 end
 
